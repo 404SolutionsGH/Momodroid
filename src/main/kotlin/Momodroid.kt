a0817551ade3c20b46dd70cfcf955db3cfe423d5
herberth arthur <herbertharthur80@gmail.com>
2024-06-22 20:47:55 +0000

    started the collections product of the API

--- src/main/kotlin/Momodroid.kt
+++ src/main/kotlin/Momodroid.kt
@@ -1,9 +1,20 @@
+import components.HelperMethods
 import momoProductSet.Collections
 
-class Momodroid(val apiKey:String) {
+class Momodroid(val subscriptionKey:String) {
 
 //    all products provided by the mtn momo APIs
 
+fun collections():Collections?{
+   return HelperMethods.tryCatchWrapper {
+        //    getting api key from Manager API(Not implemented)
+        var apiKey:String?=null
 
+
+        Collections(apiKey!!)
+    }
+
+
+}
 
 }
