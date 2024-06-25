93c7c1cb639ddeab3cabb3ee15338fb55bd6f254
herberth arthur <herbertharthur80@gmail.com>
2024-06-25 13:40:31 +0000

    Code Refactor

--- src/main/kotlin/libs/Retrofit.kt
+++ src/main/kotlin/libs/Retrofit.kt
@@ -1,11 +1,11 @@
 package libs
 
-import components.RequestMethod
+
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Retrofit(private val baseUrl:String) {
 
-    fun config(requestMethods:Class<RequestMethod>):RequestMethod{
+    fun<T> config(requestMethods:Class<T>):T{
     val retrofit= retrofit2.Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build()
         return retrofit.create(requestMethods)
     }
