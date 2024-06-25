0fc778816a9a26a5d1bfd6f830345d68d2fbbbf4
herberth arthur <herbertharthur80@gmail.com>
2024-06-25 16:07:24 +0000

    Code Refactor

--- src/main/kotlin/requestMethods/AccessTokenHttpMethods.kt
+++ src/main/kotlin/requestMethods/AccessTokenHttpMethods.kt
@@ -1,12 +1,11 @@
 package requestMethods
 
-import components.RequestMethod
 import data.AccessTokenResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 
-abstract class AccessTokenHttpMethods():RequestMethod {
+abstract class AccessTokenHttpMethods() {
     @GET("/token")
     abstract fun  getToken(@Header("Authorization") apiKey:String):Call<AccessTokenResponse>
 
