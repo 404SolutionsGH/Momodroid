6185adddea59558a5f52f42d9c7794848f95ced6
herberth arthur <herbertharthur80@gmail.com>
2024-06-27 05:03:26 +0000

    Code Refactor

---
+++ src/test/kotlin/MomodroidTest.kt
@@ -0,0 +1,18 @@
+//import data.collections.RequestToPayBody
+//
+////class MomodroidTest {
+////}
+//
+//
+//
+//fun main(){
+//
+//    val momodroid= Momodroid("jdjfjfkgkgkkgkg","fgfgdhhshdhdh")
+//
+//
+//   val collection= momodroid.collections()
+//
+//    collection.requestToPay("shddjdj","shdhdhdfhfhf")
+//
+//
+//}
